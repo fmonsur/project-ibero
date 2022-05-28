2718a07f5a984cbc984ac4c193d2eb1b62183f2a
Francisco Monsalve <francisco.monsalve@digitalcontent.com.co>
2022-05-28 18:26:29 -0500

    Carga de nuevas vistas hasta listado de proyectos

---
+++ v1/home/index2.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+	<link rel="stylesheet" href="../../public/css/index2.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-3 col-md-2 col-sm-12"></div>
+			<div class="col-lg-6 col-md-8 col-sm-12">
+				<div class="card">
+					<div class="box">
+						<form>
+							<br>
+							<img src="../../public/img/logos/logo-forja.png" alt="Forja Group">
+							<br>
+							<br>
+							<p class="text-white"> Ingrese su correo y contraseña!</p> 
+							<input type="text" name="" placeholder="Correo"> 
+							<input type="password" name="" placeholder="Contraseña"> 
+							<a class="forgot" href="#">Olvide mi contraseña</a> 
+							<input type="submit" name="" value="Ingresar" href="#">
+
+						</form>
+						<div>
+							<p class="cod-acceso">
+								Tengo un código de acceso 
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-2 col-sm-12"></div>
+		</div>
+	</div>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
